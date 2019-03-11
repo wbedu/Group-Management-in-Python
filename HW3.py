7997a430b2e528f8d35f88465bb3753d191d50c3
Wilfried Bedu <will.bedu@justforcode.com>
2019-03-11 11:26:43 -0500

    minor input type validation

--- HW3.py
+++ HW3.py
@@ -10,6 +10,16 @@ options.append("4, Validate all existing groups.")
 options.append("5, Output each group’s number and members.")
 
 
+def request_int(req):
+    while True:
+        try:
+            res = int(input(req))
+            return res
+        except ValueError:
+            print("Invalid Entry type")
+
+
+
 def print_options():
     print("Options:")
     for option in options:
@@ -39,7 +49,7 @@ def create_group():
     guid = Group().id()
     while True:
         print_group_people_with_no_group()
-        uuid = int(input("Which person would you like to add to the new group?(-1 to finish adding people): "))
+        uuid = request_int("Which person would you like to add to the new group?(-1 to finish adding people): ")
         if uuid == -1:
             return
         add_member(guid, uuid)
@@ -67,7 +77,7 @@ def remove_member(guid, uuid):
 
 
 def modify_group():
-    guid = int(input("Which group would you like to modify?"))
+    guid = request_int("Which group would you like to modify?")
     group = Group.get(guid)
     if group is None:
         print("There is no group with that id")
@@ -76,7 +86,7 @@ def modify_group():
     if "add" in action:
         while True:
             print_group_people_with_no_group()
-            uuid = int(input("Which person would you like to add? (-1 to finish add people):"))
+            uuid = request_int("Which person would you like to add? (-1 to finish add people):")
             if uuid is -1:
                 return
             add_member(guid, uuid)
@@ -84,7 +94,7 @@ def modify_group():
         while True:
             for person in group.members():
                 print(person.uuid(), " ", person.name())
-            uuid = int(input("Which person would you like to remove? (-1 to finish removing people):"))
+            uuid = request_int("Which person would you like to remove? (-1 to finish removing people):")
             if uuid is -1:
                 return
             remove_member(guid, uuid)
@@ -109,7 +119,7 @@ def main_loop():
         5: print_groups
     }
     while True:
-        usr_option = int(input("Choose an option: "))
+        usr_option = request_int("Choose an option: ")
         if usr_option not in range(-1, 6):
             print("Error: invalid input")
         else:
